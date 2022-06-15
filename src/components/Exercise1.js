2002a2a3dbdf0a97a3e14a6f6bd7f4bf990c8cfa
Ana Liebert <analiebert19@gmail.com>
2022-06-15 14:03:00 -0400

    routing established

---
+++ src/components/Exercise1.js
@@ -0,0 +1,7 @@
+const Exercise1 = () => {
+    return (
+        <h1>test</h1>
+    )
+}
+
+export default Exercise1
---
+++ src/components/Exercise2.js
@@ -0,0 +1,7 @@
+const Exercise2 = () => {
+    return (
+        <h1>test two</h1>
+    )
+}
+
+export default Exercise2
