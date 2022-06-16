008746b4287fb81e0f886ae62de75bd22b0808b8
Ana Liebert <analiebert19@gmail.com>
2022-06-15 22:04:01 -0400

    problem three completed

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -1,7 +1,13 @@
+import { useState, useEffect } from "react";
+
 const Exercise1 = () => {
 
+
+    //Problem one ---------------------------------------------->
+    //Looping through the number set, finding a random index, setting the array index to a random index, then continually swapping the random index for the original indexed array, until all values have been randomized
+    //Mapping through the result to display the new array in the HTML below 
+    
     let problemOne = [44, 154, 32, 41, 7, 22, 17, 21, 9, 20];
-    let problemTwo = [22, 14, 3, 5, 667, 19, 2, 41, 9, 70];
 
     const randomize = (array) => {
         for (let i = array.length - 1; i >= 0; i--) {
@@ -11,30 +17,85 @@ const Exercise1 = () => {
             array[randomIndex] = swap;
         }
     }
-    
+    randomize(problemOne)
+
+
+
+    //Problem Two ---------------------------------------------->
+    //Looping through the number set, saving the sum of each indexed number to a variable, diving the sum by the number of numbers in the set for the mean
+    //Calling this function in the HTML below to display the result
+    let problemTwo = [22, 14, 3, 5, 667, 19, 2, 41, 9, 70];
     const findMean = (array) => {
         let sum = 0
         let mean = 0
         for (let i = 0; i < array.length; i++) {
-            console.log(array[i])
             sum += array[i]
         }
         mean = Math.floor(sum / array.length)
         return mean
     }
-    
-    randomize(problemOne)
-    
+
+
+    //Problem Three ---------------------------------------------->
+
+    //Fetching data from the given blueprint URL ----->
+    const URL = "https://blueprintinteractive.com/test.json/";
+
+    //Utilizing useState, and useEffect to update the table asychrounously ----->
+    const [table, setTable] = useState({})
+
+
+    //Utilizing an async/await function for simple syntax to fetch the data, and update the state ----->
+
+    const getData = async () => {
+        const response = await fetch(URL)
+
+        //Note** for json data I would have utilized response.json(), but it appears the URL did not contain valid json data
+        //Instead, I translated the data to text, and then tranlated the text to a javascript object array calling the Function constructor, which I found as a better alternative to eval()
+        //I attached a screenshot of the error message from the URL in the main directory ----->
+
+        const data = await response.text();
+        const objectArray = Function('"use strict";return (' + data + ')')();
+        setTable(objectArray)
+    }
+
+
+    useEffect(() => {
+        getData()
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+
     return (
         <div>
+
+            <h5>1. Randomize the number set [44, 154, 32, 41, 7, 22, 17, 21, 9, 20] --{'> '}
+                [{problemOne.map((result) => (
+                    <span> {result},</span>
+                ))}] </h5>
+
+            <h5>2. Find the mean of [22, 14, 3, 5, 667, 19, 2, 41, 9, 70] --{'> '}
+                {findMean(problemTwo)} </h5>
+
             <div>
-                <h5>1. Randomize the number set [44, 154, 32, 41, 7, 22, 17, 21, 9, 20] --{'> '}
-                    [{problemOne.map((result) => (
-                        <span> {result},</span>
-                    ))}] </h5>
-                    
-                <h5>2. Find the mean of [22, 14, 3, 5, 667, 19, 2, 41, 9, 70] --{'> '}
-                    {findMean(problemTwo)} </h5>
+                <h5>3. Fetch and parse the data --{'> '} </h5>
+                <table style={{ border: "solid", width: "500px", textAlign: "center", margin: "50px" }}>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Gender</th>
+                    </tr>
+                    {table.map((data) => {
+                        return (
+                            <tr >
+                                <td>{data.firstname}</td>
+                                <td>{data.lastname}</td>
+                                <td>{data.email}</td>
+                                <td>{data.gender}</td>
+                            </tr>
+                        )
+                    })}
+                </table>
             </div>
         </div>
     )
--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -1,38 +1,13 @@
-import {useState, useEffect} from "react";
 
 const Exercise2 = () => {
 
-    //Fetching data from the given blueprint URL ----->
-    const URL = "https://blueprintinteractive.com/test.json/";
-
-    //Utilizing useState, and useEffect to update the table asychrounously ----->
-    const [table, setTable] = useState({})
-
-    //Utilizing an async/await function for simple syntax to fetch the data, and update the state ----->
-    const getData = async () => {
-        const response = await fetch(URL)
-
-    //Note** for json data I would have utilized response.json(), but it appears the URL did not contain valid json data
-    //Instead, I translated the data to text, and then tranlated the text to a javascript object array calling the Function constructor, which I found as a better alternative to eval()
-    //I attached a screenshot of the error message from the URL in the main directory ----->
-
-        const data = await response.text();
-        const objectArray = Function('"use strict";return (' + data + ')')();
-        setTable(objectArray)
-    }
-
-    console.log("the table, " , table)
-
-    useEffect(() => {
-        getData()
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
-
-
 
     return (
-        <h1>test two</h1>
+<div>
+
+</div>
     )
+
 }
 
 export default Exercise2
