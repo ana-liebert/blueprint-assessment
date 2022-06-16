ad002be5cdd64bfdc2c40a00b4846efbcf9ebbb0
Ana Liebert <analiebert19@gmail.com>
2022-06-16 12:51:33 -0400

    initial hero styling

--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -3,31 +3,58 @@ const Exercise2 = () => {
 
 
     return (
-<body>
-    <nav>
-        <p>placeholder: navigation with logo, buttons, social icons</p>
-    </nav>
+        <body>
+            <nav>
+                <p>placeholder: navigation with logo, buttons, social icons</p>
 
-    <div>
-        <p>placeholder: hero image
-        </p>
-    </div>
-    <div>
-        <p>placeholder: overlay with logo and signup form</p>
-    </div>
+            </nav>
 
-    <div>
-    <div>
-        <p>placeholder: Donate to support</p>
-    </div>
-        <p>placeholder: about content with images</p>
-    </div>
-    
-    <footer>
-        <p>placeholder: footer</p>
-    </footer>
+            {/* hero-----------> */}
 
-</body>
+            <div id="hero">
+
+                <div>
+                    <img className="hero-img" src={process.env.PUBLIC_URL + '/Blu_developer_test/source-files/hero.png'} />
+                </div>
+
+                <div class="hero-overlay">
+                    <img class="samantha-logo" src={process.env.PUBLIC_URL + '/Blu_developer_test/source-files/SamanthaHall_Logo_reversed.png'} />
+                    <form>
+                        <div class="mb-2">
+                            <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Email" />
+                        </div>
+
+                        <div class="row mb-2">
+                            <div class="col">
+                                <input type="tel" class="form-control" placeholder="Phone" />
+                            </div>
+                            <div class="col mb2">
+                                <input type="text" class="form-control" placeholder="ZIP" />
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Sign Up</button>
+                    </form>
+                </div>
+
+            </div>
+
+            {/* hero-------------> */}
+
+            {/* about------------> */}
+            <div>
+                <div>
+                    <p>placeholder: Donate to support</p>
+                </div>
+                <p>placeholder: about content with images</p>
+            </div>
+
+            {/* about------------> */}
+
+            <footer>
+                <p>placeholder: footer</p>
+            </footer>
+
+        </body>
     )
 
 }
